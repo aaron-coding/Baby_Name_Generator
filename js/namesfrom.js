ae2c6334dad620eb3ca521b5ebb19de0a14d908a
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-11-10 13:06:01 -0800

    Style change + beginnings of last name generator

--- js/namesfrom.js
+++ js/namesfrom.js
@@ -1,7 +1,34 @@
-var namesAry = ["Daenerys", "Joffrey", "Tyrion", "Jon", "Arya", "Gregor", "Sansa",
-"Theon", "Cersei", "Jaime", "Drogo", "Eddard", "Margaery", "Robb", "Bran", "Tywin"]
+var namesAry = ["Daenerys", "Joffrey", "Tyrion", "Jon", "Arya", "Gregor", "Sansa",        
+"Theon", "Cersei", "Jaime", "Drogo", "Eddard", "Margaery", "Robb", "Bran", "Tywin"]        
+        
+var OrangeNames = ["Alex", "Tiffany", "George", "Piper", "Suzanne", "Dayanara",
+"Galina", "Lorna", "Nicky", "Poussey", "Tricia", "Carrie", "John", "Tasha",
+"Rosa", "Claudette"]
+        
+function newName( j ) {        
+  var randomNum = Math.floor(Math.random()*namesAry.length);        
+  j.text( namesAry[randomNum] );        
+}       
+function newLastName( j ) {        
+  var randomNum = Math.floor(Math.random()*namesAry.length);        
+  j.text( namesAry[randomNum] );        
+}        
+        
 
-function newName( j ) {
-  var randomNum = Math.floor(Math.random()*namesAry.length);
-  j.text( namesAry[randomNum] );
-}
+// Alex         Vause        
+// Tiffany                 Doggett        
+// George         Mendez        
+// Piper Chapman                
+// Suzanne         Warren        
+// Dayanara         Diaz        
+// Galina         Reznikov        
+// Lorna         Morello        
+// Nicky         Nichols        
+// Poussey         Washington        
+// Tricia         Miller        
+// Carrie         Black        
+// John         Bennett        
+// Tasha         Jefferson        
+// Rosa Cisneros                
+// Claudette         Pelage        
+        
