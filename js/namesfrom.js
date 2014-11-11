55bab09e9fbb59b30c7470b7c0beb8d6eb5ffec7
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-11-11 13:15:37 -0800

    Add Orange is the new black name picker

--- js/namesfrom.js
+++ js/namesfrom.js
@@ -1,14 +1,22 @@
-var namesAry = ["Daenerys", "Joffrey", "Tyrion", "Jon", "Arya", "Gregor", "Sansa",        
+var gameOfThronesNames = ["Daenerys", "Joffrey", "Tyrion", "Jon", "Arya", "Gregor", "Sansa",        
 "Theon", "Cersei", "Jaime", "Drogo", "Eddard", "Margaery", "Robb", "Bran", "Tywin"]        
         
-var OrangeNames = [["Alex", "Vause"], ["Tiffany", "Doggett"], ["George", "Mendez"],
- "Piper", "Suzanne", "Dayanara", "Galina", "Lorna", "Nicky", "Poussey", "Tricia", 
- "Carrie", "John", "Tasha",
-"Rosa", "Claudette"]
+var orangeNames = [["Alex", "Vause"], ["Tiffany", "Doggett"], ["George", "Mendez"],
+ ["Piper", "Chapman"], ["Suzanne", "Warren"], ["Dayanara","Diaz"], ["Galina", "Reznikov"], ["Lorna", "Morello"]
+ , ["Nicky", "Nichols"], ["Poussey", "Washington"], ["Tricia", "Miller"], ["Carrie", "Black"], ["John", "Bennett"], 
+ ["Tasha", "Jefferson"], ["Rosa", "Cisneros"], ["Claudette", "Pelage"]]
         
-function newName( j ) {        
-  var randomNum = Math.floor(Math.random()*namesAry.length);        
-  j.text( namesAry[randomNum] );        
+function newName( j, num ) {        
+
+  console.log(num)   
+  if (num === 1) { 
+    var randomNum = Math.floor(Math.random()*gameOfThronesNames.length); 
+    j.text( gameOfThronesNames[randomNum] );            
+  } else {
+    var randomNum = Math.floor(Math.random()*orangeNames.length); 
+    j.text( orangeNames[randomNum][0] ); 
+  }
+
 }       
 // function newLastName( j ) {
 //   var randomNum = Math.floor(Math.random()*namesAry.length);
@@ -25,7 +33,7 @@ function newName( j ) {
 // Alex         Vause        
 // Tiffany                 Doggett        
 // George         Mendez        
-// Piper Chapman                
+// Piper          Chapman                
 // Suzanne         Warren        
 // Dayanara         Diaz        
 // Galina         Reznikov        
